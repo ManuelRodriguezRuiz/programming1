cb507c8e1f9fa8cdfebfd10d669b77374e8c7de3
Manuel Rodriguez <manuelrodriguezruiz@student.hu.nl>
2018-11-09 15:27:43 +0100

    opdracht

---
+++ Les13/1. XML-bestanden schrijven.py
@@ -0,0 +1,8 @@
+pip install xmltodict
+import xmltodict
+
+with open("artikelen.xml", "r") as f:
+    xmldict = xmltodict.parse(f.read())
+
+    for artikel in xmldict["artikelen"]["artikel"]:
+        print(artikel["naam"])
