420280fc03ce5532411f3d49210075d34ba944e1
Manuel Rodriguez <manuelrodriguezruiz@student.hu.nl>
2018-11-09 15:23:20 +0100

    opdracht

---
+++ Les10/4. Containers.py
@@ -0,0 +1,5 @@
+            geordend    Muteerbaar  Iterable    Dubbele waarden toegestaan
+Tuple       ja          nee         ja          ja
+Dictionary  nee         ja          ja          nee
+Set         nee         ja          nee         nee
+List        ja          ja          ja          ja
